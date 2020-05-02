a593552bba0cf162df385ef8b86f6ae2db03d5cd
clwclwclw <729701285@qq.com>
2020-05-02 18:57:46 +0800

    reason

---
+++ app/src/main/java/cn/edu/sdwu/android02/home/sn170507180230/HomeworkActivity.java
@@ -0,0 +1,24 @@
+package cn.edu.sdwu.android02.home.sn170507180230;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeworkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_homework);
+
+    }
+    public void homework1(View view){
+        Intent intent=new Intent(this,HomeworkActivity1.class);
+        startActivity(intent);
+    }
+    public void homework2(View view){
+        Intent intent=new Intent(this,HomeworkActivity2.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/cn/edu/sdwu/android02/home/sn170507180230/HomeworkActivity2.java
@@ -0,0 +1,12 @@
+package cn.edu.sdwu.android02.home.sn170507180230;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeworkActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
